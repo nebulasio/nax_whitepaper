8d483d479cee9a0da03c662957bce4c09a611f3e
Zhuoer Wang <zhuoerwang@gmail.com>
2019-07-29 13:44:33 +0800

    update vision

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
-#cd ./ch && xelatex main.tex && biber main && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
 cd ./zh && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
 cd ../
 cp ./zh/main.pdf white_paper_zh.pdf
-cd ./en && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
+#cd ./en && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
+#cd ../
+#cp ./en/main.pdf white_paper_en.pdf
+
+
+cd ./zh && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
 cd ../
-cp ./en/main.pdf white_paper_en.pdf
+cp ./zh/main.pdf white_paper_zh.pdf
+
+#cd ./en && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
+#cd ../
+#cp ./en/main.pdf white_paper_en.pdf
