d19761b9006359b55c004675fe96ee4f62d62af4
Zhuoer Wang <zhuoerwang@gmail.com>
2019-08-11 03:04:48 +0800

    update pdf build and citation

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
-cd ./zh && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
-cd ../
-cp ./zh/main.pdf dist/white_paper_zh.pdf
-#cd ./en && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
-#cd ../
-#cp ./en/main.pdf dist/white_paper_en.pdf
-
-
-cd ./zh && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
+cd ./zh && xelatex main.tex && bibtex main.aux && xelatex main.tex && xelatex main.tex
 cd ../
 cp ./zh/main.pdf dist/white_paper_zh.pdf
 
-#cd ./en && xelatex main.tex && makeindex main.nlo -s nomencl.ist -o main.nls && xelatex main.tex
+
+#cd ./en && xelatex main.tex && bibtex main.aux && xelatex main.tex && xelatex main.tex
 #cd ../
 #cp ./en/main.pdf dist/white_paper_en.pdf
